c3150ad33788180d211475afbc1af47554e996ac
nihaprezz <sergio13prez@gmail.com>
2019-10-12 22:53:43 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#   coupons table needs coupon_code and store columns which should both be string 
+#$ rails generate migration AddPartNumberToProducts 
+
+
+coupon1 = Coupon.find_or_create_by(coupon_code: 12, store: 100)
+coupon2 = Coupon.find_or_create_by(coupon_code: 14, store: 200)
